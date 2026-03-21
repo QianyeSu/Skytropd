c3495a69e6a0b88e3eabc1123128075de0827020
Qianye Su <976113615@qq.com>
2026-03-21 22:36:07 +0800

    ci: update Python version in documentation config and requirements, add theme overrides for improved styling

--- skytropd/docs/source/conf.py
+++ skytropd/docs/source/conf.py
@@ -136,17 +136,24 @@ html_theme = 'classic'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'rightsidebar': 'false',
+    'stickysidebar': 'false',
+    'collapsiblesidebar': 'true',
+    'body_min_width': '760px',
+    'body_max_width': '960px',
+    'sidebarwidth': '230px',
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = 'SkyTropD Documentation'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+html_short_title = 'SkyTropD'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
@@ -161,6 +168,8 @@ html_theme = 'classic'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = ['theme_overrides.css']
+html_sidebars = {'**': ['globaltoc.html', 'relations.html', 'searchbox.html']}
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
@@ -293,5 +302,6 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
+
 
 
