307829196ed4922bcb5d4b1e7c874c1532ac3e02
Qianye Su <976113615@qq.com>
2026-05-06 01:26:38 +0800

    refactor: remove unused Fortran zero crossing status check from smoke_wheel.py

--- testing/smoke_wheel.py
+++ testing/smoke_wheel.py
@@ -4,7 +4,6 @@ import numpy as np
 
 import skytropd as pyt
 import skytropd.tutorial as tutorial
-from skytropd._fortran_zero_crossing import fortran_zero_crossing_status
 
 
 def main() -> None:
@@ -13,9 +12,6 @@ def main() -> None:
     assert (package_dir / "ValidationData" / "va.nc").exists()
     assert (package_dir / "ValidationMetrics" / "EDJ.nc").exists()
     assert tutorial.V.shape == (tutorial.lat.size, tutorial.lev.size)
-
-    backend_ok, backend_error = fortran_zero_crossing_status()
-    assert backend_ok, backend_error
 
     lat = np.array([10.0, 20.0, 30.0, 40.0, 50.0])
     field = np.sin(2.0 * np.radians(lat))
